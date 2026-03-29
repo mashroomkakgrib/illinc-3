569fb4875cffa1664163902c299a76965c09a0ee
masha <DrachevaMaVa@yandex.ru>
2026-03-28 18:22:17 -0700

    edits

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -8,21 +8,39 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(updateClock, 1000);
   updateClock();
 
-  const button1 = document
-    .getElementById('button1')
-    .addEventListener('click', function () {
-      const target = document.getElementById('film1');
-    });
+  const button1 = document.getElementById('button1');
+  button1.addEventListener('click', function () {
+    const target = document.getElementById('film1');
+    target.style.display = 'block';
+  });
 
-  const button2 = document
-    .getElementById('button2')
-    .addEventListener('click', function () {
-      const target = document.getElementById('film2');
-    });
+  const button2 = document.getElementById('button2');
+  button2.addEventListener('click', function () {
+    const target = document.getElementById('film2');
+    target.style.display = 'block';
+  });
 
-  const button3 = document
-    .getElementById('button3')
-    .addEventListener('click', function () {
-      const target = document.getElementById('film3');
-    });
+  const button3 = document.getElementById('button3');
+  button3.addEventListener('click', function () {
+    const target = document.getElementById('film3');
+    target.style.display = 'block';
+  });
+
+  const buttoninfilm1 = document.getElementById('buttoninfilm1');
+  buttoninfilm1.addEventListener('click', function () {
+    const target = document.getElementById('film1');
+    target.style.display = 'none';
+  });
+
+  const buttoninfilm2 = document.getElementById('buttoninfilm2');
+  buttoninfilm2.addEventListener('click', function () {
+    const target = document.getElementById('film2');
+    target.style.display = 'none';
+  });
+
+  const buttoninfilm3 = document.getElementById('buttoninfilm3');
+  buttoninfilm3.addEventListener('click', function () {
+    const target = document.getElementById('film3');
+    target.style.display = 'none';
+  });
 });
