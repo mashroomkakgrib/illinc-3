db82d22854b96cd71fc4086ab655bc7e25a84782
masha <DrachevaMaVa@yandex.ru>
2026-03-28 19:19:50 -0700

    adding drag and drop mechanics

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.buttons');
-
   function updateClock() {
     const now = new Date();
     document.getElementById('clock').innerText = now.toLocaleTimeString();
@@ -8,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   setInterval(updateClock, 1000);
   updateClock();
 
+  document.querySelector('.buttons');
   const button1 = document.getElementById('button1');
   button1.addEventListener('click', function () {
     const target = document.getElementById('film1');
@@ -42,5 +41,38 @@ document.addEventListener('DOMContentLoaded', function () {
   buttoninfilm3.addEventListener('click', function () {
     const target = document.getElementById('film3');
     target.style.display = 'none';
+  });
+});
+
+let isDragging = false;
+let activeElement = null;
+let startX = 0,
+  startY = 0;
+
+document.querySelectorAll('.dragg').forEach((img) => {
+  img.ondragstart = () => false;
+
+  img.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+    isDragging = true;
+    activeElement = img;
+
+    startX = e.clientX - activeElement.offsetLeft;
+    startY = e.clientY - activeElement.offsetTop;
+
+    activeElement.style.position = 'absolute';
+    activeElement.style.zIndex = '1000';
+  });
+
+  document.addEventListener('mousemove', (e) => {
+    if (isDragging && activeElement) {
+      activeElement.style.left = e.clientX - startX + 'px';
+      activeElement.style.top = e.clientY - startY + 'px';
+    }
+  });
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false;
+    activeElement = null;
   });
 });
